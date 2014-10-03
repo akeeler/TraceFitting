cd25176fee1ba9c292267fddfc32fe1c1af1d05f
S.V. Paulauskas <stanpaulauskas@gmail.com>
2014-10-03 12:10:53 -0400

    Adding in copyright info

--- inc/Trace.hpp
+++ inc/Trace.hpp
@@ -1,4 +1,21 @@
-/** \file TraceHandler.hpp
+ /**************************************************************************
+  *  Copyright S. V. Paulauskas 2014                                       *
+  *                                                                        *
+  *  This program is free software: you can redistribute it and/or modify  *
+  *  it under the terms of the GNU General Public License as published by  *
+  *  the Free Software Foundation, version 3.0 License.                    *
+  *                                                                        *
+  *  This program is distributed in the hope that it will be useful,       *
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+  *  GNU General Public License for more details.                          *
+  *                                                                        *
+  *  You should have received a copy of the GNU General Public License     *
+  *  along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+  **************************************************************************
+*/
+
+/*! \file TraceHandler.hpp
  *  \brief A class to handle the processing of traces
  *  \author S. V. Paulauskas
  *  \date 27 March 2014
